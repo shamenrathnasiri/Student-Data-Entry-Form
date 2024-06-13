014c92bb4be0ab24fcb25cc47644a915c3dc2fd6
Bhanuka Shamen Rathnasiri <120768729+shamenrathnasiri@users.noreply.github.com>
2024-06-13 17:38:36 +0530

    Update StDetails.py
    create a clear button when the click for all details are clear.

--- StDetails.py
+++ StDetails.py
@@ -2,12 +2,19 @@
 import tkinter as tk
 from tkinter import ttk
 
-
 root =tk.Tk()
 root.title("Student data entry form - University of SYYZ")
 root.geometry('800x600')
 root.resizable(False,False)
- 
+
+def clear():
+    nameEntry.delete(0,tk.END)
+    adressEntry.delete(0,tk.END)
+    ageEntry.delete(0,tk.END)
+    genderEntry.delete(0,tk.END)
+    mobileEntry.delete(0,tk.END)
+    emailEntry.delete(0,tk.END)
+
 tk.Label(root,text="Student Registration Form", font="arial 25").pack(pady=50)
 tk.Label(text="Name", font =23).place(x=100,y=150)
 tk.Label(text="Adress", font =23).place(x=100,y=200)
@@ -27,29 +34,24 @@ emailValue= tk.StringVar()
 nameEntry=tk.Entry(root,textvariable=nameValue,width=30,bd=2,font=20)
 adressEntry=tk.Entry(root,textvariable=adressValue,width=30,bd=2,font=20)
 ageEntry=tk.Entry(root,textvariable=ageValue,width=30,bd=2,font=20)
-genderEntry1=tk.Radiobutton(root,text="Male",font='arial 14',value='male',variable=genderValue)
-genderEntry2=tk.Radiobutton(root,text="Female",font='arial 14',value='Female',variable=genderValue)
+genderEntry=tk.Entry(root,textvariable=genderValue,width=30,bd=2,font=20)
 mobileEntry=tk.Entry(root,textvariable=mobileValue,width=30,bd=2,font=20)
 emailEntry=tk.Entry(root,textvariable=emailValue,width=30,bd=2,font=20)
 
 nameEntry.place(x=200,y=150)
 adressEntry.place(x=200,y=200)
 ageEntry.place(x=200,y=250)
-genderEntry1.place(x=200,y=300)
-genderEntry2.place(x=300,y=300)
+genderEntry.place(x=200,y=300)
 mobileEntry.place(x=200,y=350)
 emailEntry.place(x=200,y=400)
 
-def register():
-    tk.Label(text="registration Sucsess",fg='green',font=('calibri',11), ).place(x=250,y=430)
-
 #submit and clear button
 
-button1 =ttk.Button(root,text='Submit',command=register) #submit button
+button1 =ttk.Button(root,text='Submit') #submit button
 button1.pack()
 button1.place(x=270,y=500)
 
-button2=ttk.Button(root,text=' X Clear') #clear button
+button2=ttk.Button(root,text=' X Clear', command= clear) #clear button
 button2.pack()
 button2.place(x=470,y=500)
 
