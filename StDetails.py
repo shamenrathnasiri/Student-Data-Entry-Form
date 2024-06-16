76372f555fdd6a95e2898b172da30d0233c3c4db
Bhanuka Shamen Rathnasiri <120768729+shamenrathnasiri@users.noreply.github.com>
2024-06-16 15:33:14 +0530

    Update StDetails.py
    today , connect with mysql database.

--- StDetails.py
+++ StDetails.py
@@ -1,13 +1,37 @@
-
 import tkinter as tk
 from tkinter import ttk
+import mysql.connector 
+import tkinter.messagebox as msg
+
 
 root =tk.Tk()
 root.title("Student data entry form - University of SYYZ")
 root.geometry('800x600')
 root.resizable(False,False)
 
-def clear():
+#add database
+def regdata():
+    mydb = mysql.connector.connect(
+        host ="localhost",
+        port = "3306",
+        user ="root",
+        password ="",
+        database ="stdreg")
+    
+    mycursor = mydb.cursor()
+
+    name = nameEntry.get()
+    adress = adressEntry.get()
+    age = ageEntry.get()
+    gender = genderEntry.get()
+    mobile = mobileEntry.get()
+    email = emailEntry.get()
+
+    mycursor.execute("insert into stdetails values(%s,%s,%s,%s,%s,%s)",(name,adress,age,gender,mobile,email))
+    mydb.commit()
+    msg.showinfo("registration","registeres successfully")
+
+def clear(): #clear 
     nameEntry.delete(0,tk.END)
     adressEntry.delete(0,tk.END)
     ageEntry.delete(0,tk.END)
@@ -47,12 +71,14 @@ emailEntry.place(x=200,y=400)
 
 #submit and clear button
 
-button1 =ttk.Button(root,text='Submit') #submit button
+button1 =ttk.Button(root,text='Submit', command= regdata ) #submit button
 button1.pack()
 button1.place(x=270,y=500)
 
 button2=ttk.Button(root,text=' X Clear', command= clear) #clear button
 button2.pack()
 button2.place(x=470,y=500)
+
+
 
 root.mainloop()
